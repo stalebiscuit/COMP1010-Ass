ef9c14593a6b2f4dc0b934c73545b55c95aa1a72
frankpearson01 <frank.pearson@students.mq.edu.au>
2024-10-10 14:14:35 +1100

    2balls

--- COMP1010/src/App.java
+++ COMP1010/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("balls");
+        System.out.println("balls"); // 2 balls
     }
 }
