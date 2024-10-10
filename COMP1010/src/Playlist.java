71fc7a8600132f5d3afb1421fd5d436b9b4b365c
tomhann <161672535+tomhann@users.noreply.github.com>
2024-10-10 14:36:18 +1100

    added stuff to each class

--- COMP1010/src/Playlist.java
+++ COMP1010/src/Playlist.java
@@ -1,3 +1,15 @@
+import java.util.*;
+
 public class Playlist {
+ArrayList<Song> playlistContents;
+String name;
+
+public Playlist(){
+    return 
+}
+
+public void addSong(Song a){
+    playlistContents.add(a);
+}
 
 }
