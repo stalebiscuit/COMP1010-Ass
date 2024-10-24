0a3000061eac6ef08e9df2fb3ce21d229add7fec
tomhann <161672535+tomhann@users.noreply.github.com>
2024-10-24 22:41:10 +1100

    Update App.java

--- COMP1010/src/App.java
+++ COMP1010/src/App.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+      public static void main(String[] args) throws Exception {
           Scanner scanner = new Scanner(System.in);
 
         // Create some initial songs
